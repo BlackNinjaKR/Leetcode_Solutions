a88e2fbc897bce593cced8351944b2f54698cc88
Anik Dasgupta <Dasguptaanik123@gmail.com>
2025-07-21 09:27:21 +0530

    Length of Last Word of a String

---
+++ C&C++/Length_Last_Word.c
@@ -0,0 +1,53 @@
+/*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.
+ */
+#include<stdio.h>
+#include<string.h>
+
+int LengthOfLastWord(char s[])
+{
+    int counter = 0;
+    for (int i = strlen(s)-1; i>=0; i--)
+    {
+        if (s[i] == ' ' && counter == 0)
+            continue;
+        else if (s[i]!=' ')
+            counter++;
+        else
+            break;        
+    }
+    return counter;
+}
+int main()
+{
+    char str[] = "Hello World";
+    printf("%d",LengthOfLastWord(str));
+    return 0;
+}
